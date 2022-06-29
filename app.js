b88804472ca062eafcdae21d88ed2868468bdfb6
Kilimba <dyaul003@umn.edu>
2022-06-29 17:25:19 -0500

    json packages

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+console.log(inquirer);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-console.log(profileDataArgs);
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-console.log(name, github);
 
-const pageHTML = generatePage(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+
+// });
